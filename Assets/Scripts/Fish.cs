23ab4e4e11f73ab98dd5a1b01539863c9c2291da
bariserdem <bariserdem@panteon.games>
2023-05-24 20:56:02 +0300

    Added little Fish Movement
    Added little Fish Movement

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float speed;
+    int angle;
+    int maxAngle = 20;
+    int minAngle = -60;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, speed);
+        }
+
+        if (_rb.velocity.y > 0)
+        {
+            if (angle <= maxAngle)
+            {
+                angle = angle + 4;
+            }
+        }
+        else if (_rb.velocity.y < -2.5f)
+        {
+            if (angle > minAngle)
+            {
+                angle = angle - 2;
+            }
+        }
+        transform.rotation = Quaternion.Euler(0, 0, angle); 
+    }
+}
